a4f1f422c44abdcda3294c5a7cb1a5d5ab6a3df4
Michał Głodowski <s10552@pjwstk.edu.pl>
2024-10-11 21:23:42 +0200

    rewriting 3-4

--- LAB3/index.php
+++ LAB3/index.php
@@ -1,94 +1,91 @@
 <html>
-<p> zadanie 3</p>
-<?php
-function yobro()
-{
-$enter = 0;
+    <p> zadanie 3</p>
+    <?php
+    function yobro() {
+        $enter = 0;
+        $data = " dzem jest niebieski. ";
+        
+        foreach (count_chars ($data, 1) as $i => $val) {
+            if ($enter == 1) {
+                $enter = 0;
+                continue;
+            }
 
-$data = " dzem jest niebieski. ";
-foreach (count_chars ($data, 1) as $i => $val)
-    {
-        if ($enter == 1)
-        {
-            $enter = 0;
-            continue;
-        }
-        if (chr ($i) == "\n")
-        {
-            echo "<br>There are $val instance(s) of \" Enter \" in the string.";
-            $enter = 1;
-        }
-        else
-        {
-            echo "<br>There are $val instance(s) of \"" , chr ($i) , "\" in the string.";
+            if (chr ($i) == "\n") {
+                echo "<br>There are $val instance(s) of \" Enter \" in the string.";
+                $enter = 1;
+            } else {
+                echo "<br>There are $val instance(s) of \"" , chr ($i) , "\" in the string.";
+            }
         }
     }
-}
 
-yobro();
-echo "<br></br>---K-o-n-w-e-r-t-e-r--s-z-y-f-r-u--C-e-z-a-r-a---<br></br>";
-function Cezar($text, $offset)
-{
-$s = $_POST['s'];
-$k = $_POST['k'];
-$i=0;
-if ($s and is_numeric($k)) {
-$l = strlen ($s);
-while ( $l>$i){
-$z1 = ord(substr($s, $i, 1));
-if(($z1>=97)&&($z1<=122)){
-$z1=$z1+$k;
-if($z1<97){
-$z1=97-$z1;
-$z1=123-$z1;
-} else
-if($z1>122){
-$z1=$z1-122;
-$z1=96+$z1;
-}} else
-if(($z1>=65)&&($z1<=90)){
-$z1=$z1+$k;
-if($z1<65){
-$z1=65-$z1;
-$z1=91-$z1;
-} else
-if($z1>90){
-$z1=$z1-90;
-$z1=64+$z1;
-}}
+    yobro();
+    
+    echo "<br></br>---K-o-n-w-e-r-t-e-r--s-z-y-f-r-u--C-e-z-a-r-a---<br></br>";
+    function Cezar($text, $offset) {
+        $s = $_POST['s'];
+        $k = $_POST['k'];
+        $i = 0;
+        
+        if ($s and is_numeric($k)) {
+            $l = strlen ($s);
+            
+            while ($l > $i) {
+                $z1 = ord(substr($s, $i, 1));
 
-$z2 = chr($z1); 
-$w .= $z2; 
-$i++; 
+                if (($z1 >= 97) && ($z1 <= 122)) {
+                    $z1 = $z1 + $k;
 
-}
+                    if ($z1 < 97) {
+                        $z1 = 97 - $z1;
+                        $z1 = 123 - $z1;
+                    } else { 
+                        if ($z1 > 122) {
+                            $z1 = $z1 - 122;
+                            $z1 = 96 + $z1;
+                        }
+                    }
+                } else {
+                    if (($z1 >= 65) && ( $z1 <= 90)) {
+                        $z1 = $z1 + $k;
 
-}
-//----------------------------
-$cenzura =array('dupa', 'kurewa', 'mufiny', 'debil', 'pierdole');
-$ile = count($cenzura);
+                        if ($z1 < 65) {
+                            $z1= 65 - $z1;
+                            $z1= 91 - $z1;
+                        } else {
+                            if($z1 > 90) {
+                                $z1 = $z1 - 90;
+                                $z1 = 64 + $z1;
 
-for ($i=0; $i<$ile; $i++)
-{
-  if (strstr($s, $cenzura[$i]));
-  $dlugosc = strlen($cenzura[$i]);
-  $s = str_replace($cenzura[$i], substr($cenzura[$i], 0, 1). '***'.substr($cenzura[$i], $dlugosc-1), $s);
-}
-//----------------------------
+                            }
+                        }
+                    }
+                }
+            }
+            $z2 = chr($z1); 
+            $w .= $z2; 
+            $i++; 
+        }
+        //----------------------------
+        $cenzura = array('dupa', 'kurewa', 'mufiny', 'debil', 'pierdole');
+        $ile = count($cenzura);
 
-
-print '<FORM ACTION="" METHOD=POST>';
-print '<input type="text" name="s"> Podaj text<br>';
-print '<input type="text" name="k"> Klucz<br>';
-print '<INPUT TYPE="submit" VALUE="Koduj"><br>';
-print "<br></br> Po zakodowaniu <b>' $s '</b> z kluczem <b>' $k '</b> to : <b>' $w '</b> <br> ";
-}
-Cezar();
-//---------------------------------------------------------------------------------------------------------
-
-
-
-//----------------------------------------------------------------------------------------------------------
-?>
-
+        for ($i = 0; $i < $ile; $i++) {
+            if (strstr($s, $cenzura[$i]));
+            $dlugosc = strlen($cenzura[$i]);
+            $s = str_replace($cenzura[$i], substr($cenzura[$i], 0, 1). '***'.substr($cenzura[$i], $dlugosc - 1), $s);
+        }
+        //----------------------------
+        print '<FORM ACTION="" METHOD=POST>';
+        print '<input type="text" name="s"> Podaj text<br>';
+        print '<input type="text" name="k"> Klucz<br>';
+        print '<INPUT TYPE="submit" VALUE="Koduj"><br>';
+        print "<br></br> Po zakodowaniu <b>' $s '</b> z kluczem <b>' $k '</b> to : <b>' $w '</b> <br> ";
+    }
+    
+    Cezar();
+    //---------------------------------------------------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------
+    ?>
 </html>
--- LAB4/index.php
+++ LAB4/index.php
@@ -1,38 +1,37 @@
 <html>
-<?php
+	<?php
+	function table2string($tab) { 
+		$string = " "; 
+		$string_end = " ";
 
-function table2string($tab){
-$string = " "; $string_end = " ";
+		foreach ($tab as $i => $row) {
+			foreach ($row as $j => $cell) {
+					$string[$j] = decbin($cell);
+			} 
+			$string_end[$i] = dechex(bindec($string));
+		} 
 
-
-foreach($tab as $i => $row){
-	foreach($row as $j => $cell){
-			$string[$j] = decbin($cell);
-} 
-$string_end[$i] = dechex(bindec($string));
-} 
-return $string_end;
-}
-
-$tab = array(); $tab1= array();
-
-for ($i=0; $i<15; $i++){
-	for ($j=0; $j<15; $j++){
-	  $tab[$j]=rand(0,1);
-	 //echo "$tab[j]<br>";  
+		return $string_end;
 	}
-	$tab1[$i] = $tab;
 
-}
-foreach($tab1 as $row){
-	foreach($row as $cell){
-		echo "$cell";
+	$tab = array(); 
+	$tab1= array();
+
+	for ($i = 0; $i < 15; $i++) {
+		for ($j = 0; $j < 15; $j++) {
+			$tab[$j] = rand(0,1);
+			//echo "$tab[j]<br>";  
+		}
+		$tab1[$i] = $tab;
+	}
+
+	foreach ($tab1 as $row) {
+		foreach ($row as $cell) {
+			echo "$cell";
 		}
 		echo "<br>";
-}
-
+	}
 	$yellowswag = $table2string($tab1);
-		echo " <br>$yellowswag<br>";
-
-?>
+	echo " <br>$yellowswag<br>";
+	?>
 </html>
